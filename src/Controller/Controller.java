43c4fc9fecc0a715f5f061b212a05ae41fa493ff
Salo1802 <salo1802@hotmail.com>
2021-04-25 13:57:39 -0500

    sombra boton
    puse que e pnga una sombra sobre el boton seleccionado

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -17,18 +17,30 @@ public class Controller {
 	}
 	
 	public void draw(PApplet app) {
-		
+		logica.OrdenarAtributo(button);
 		logica.draw(app);
 		app.noStroke();
 		app.fill(94,33,41,60);
 		switch (button) {
 		case 0:
 		app.rect(150,40 , 75, 90);
+		break;
+		case 1:
+			app.rect(315,40 , 240, 90);
+			break;
+		case 2:
+			app.rect(630,40 , 370, 90);
+			break;
+		case 3:
+			app.rect(960,40 , 270, 90);
+			break;
+		case 4:
+			app.rect(1150,40 , 85, 90);
+			break;
 		}
 	}
 	
 	public void Buttons(int mousex, int mousey) {
-		logica.OrdenarAtributo(button);
 		if (mousey > 0 && mousey < 100) {
 			if(mousex>100 && mousex<190) {button = 0;}
 			if(mousex>190 && mousex<440) {button = 1;}
